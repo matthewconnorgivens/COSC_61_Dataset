7be7d63cc1ea29c728eb3b269fc5f0e1fa737f07
Matthew Givens <121989218+matthewconnorgivens@users.noreply.github.com>
2023-08-27 23:27:37 -0400

    Add files via upload

---
+++ enhancement_table_script.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `enhancement_table` (
+  `case_id` int NOT NULL,
+  `latitude` varchar(50) COLLATE utf8mb3_bin DEFAULT NULL,
+  `longitude` varchar(50) COLLATE utf8mb3_bin DEFAULT NULL,
+  `case_name` varchar(255) COLLATE utf8mb3_bin DEFAULT NULL,
+  `summary` varchar(500) COLLATE utf8mb3_bin DEFAULT NULL,
+  PRIMARY KEY (`case_id`),
+  CONSTRAINT `enhancement_id` FOREIGN KEY (`case_id`) REFERENCES `shooting_long_lat` (`case_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+INSERT INTO enhancement_table (case_id, latitude, longitude)
+SELECT case_id, longitude, latitude
+FROM shooting_long_lat;
+
+UPDATE enhancement_table et
+JOIN summary s ON et.case_id = s.case_id
+SET et.summary = s.summary;
+
+UPDATE enhancement_table et
+JOIN shooting s ON et.case_id = s.case_id
+SET et.case_name = s.case_name;
